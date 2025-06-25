c0344e2002aa9f83b9ef21394d2af3ec7d1de08c
Neil Casas <neilalfonzcasas@gmail.com>
2025-06-25 15:56:42 +0800

    fix: fixed author id typing on controller

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -5,7 +5,7 @@ import { UpdateAuthorDto } from './dto/update-author.dto';
 
 @Controller('authors')
 export class AuthorsController {
-  constructor(private readonly authorsService: AuthorsService) {}
+  constructor(private readonly authorsService: AuthorsService) { }
 
   @Post()
   create(@Body() createAuthorDto: CreateAuthorDto) {
@@ -19,16 +19,16 @@ export class AuthorsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.authorsService.findOne(+id);
+    return this.authorsService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAuthorDto: UpdateAuthorDto) {
-    return this.authorsService.update(+id, updateAuthorDto);
+    return this.authorsService.update(id, updateAuthorDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.authorsService.remove(+id);
+    return this.authorsService.remove(id);
   }
 }
