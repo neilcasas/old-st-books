fc7d0a57bb5c7136b9c496c86e574ee0045383af
Neil Casas <neilalfonzcasas@gmail.com>
2025-06-27 10:15:37 +0800

    feat: integrated storage service to authors

--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,33 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
-import { StorageService } from '@app/storage';
+import { AuthorsStorageService } from '@app/authors-storage';
+
 
 @Injectable()
 export class AuthorsService {
-  constructor(private readonly storageService: StorageService) {}
+  constructor(private readonly authorStorageService: AuthorsStorageService) { }
 
   create(createAuthorDto: CreateAuthorDto) {
-    return this.storageService.createAuthor(createAuthorDto);
+    return this.authorStorageService.createAuthor(createAuthorDto);
   }
 
   findAll() {
-    return this.storageService.getAuthors();
+    return this.authorStorageService.getAuthors();
   }
 
   findOne(id: string) {
-    return this.storageService.getAuthor(id);
+    return this.authorStorageService.getAuthor(id);
   }
 
   update(id: string, updateAuthorDto: UpdateAuthorDto) {
-    return this.storageService.updateAuthor(id, updateAuthorDto);
+    return this.authorStorageService.updateAuthor(id, updateAuthorDto);
   }
 
   remove(id: string) {
-    return this.storageService.deleteAuthor(id);
+    return this.authorStorageService.deleteAuthor(id);
   }
 
+  /*
   getBooks(id: string) {
-    return this.storageService.getBooksFromAuthor(id);
+    return this.authorStorageService.getBooksFromAuthor(id);
   }
+  */
 }
