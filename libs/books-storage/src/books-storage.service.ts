fe09c10ee333b671109593266bf15c1ad4298f91
Neil Casas <neilalfonzcasas@gmail.com>
2025-06-27 10:10:59 +0800

    feat: integrated books storage to books service

--- libs/books-storage/src/books-storage.service.ts
+++ libs/books-storage/src/books-storage.service.ts
@@ -3,8 +3,6 @@ import { CreateBookDto } from 'src/books/dto/create-book.dto';
 import { UpdateBookDto } from 'src/books/dto/update-book.dto';
 import { Book, Genre } from 'libs/types';
 import { v4 as uuidv4 } from 'uuid';
-import { BookNotFoundException } from 'src/exceptions/book-not-found.exception';
-import { DuplicateRecordException } from 'src/exceptions/duplicate-record.exception';
 
 @Injectable()
 export class BooksStorageService {
@@ -29,9 +27,6 @@ export class BooksStorageService {
 
   getBook(id: string) {
     const book = this.books.find((book) => book.id === id);
-    if (!book) {
-      throw new BookNotFoundException({ bookId: id });
-    }
     return book;
   }
 
@@ -41,27 +36,19 @@ export class BooksStorageService {
   }
 
   createBook({ name, genre, pages }: CreateBookDto) {
-    if (!this.getBookByName(name)) {
-      const newBook = {
-        id: uuidv4(),
-        name,
-        genre,
-        pages,
-      };
-      this.books.push(newBook);
-      return newBook;
-    }
-    throw new DuplicateRecordException();
+    const newBook = {
+      id: uuidv4(),
+      name,
+      genre,
+      pages,
+    };
+    this.books.push(newBook);
+    return newBook;
   }
 
-  deleteBook(id: string) {
-    const toBeRemoved = this.getBook(id);
-    if (toBeRemoved) {
-      this.books = this.books.filter((book) => book.id !== toBeRemoved.id);
-      return toBeRemoved;
-    } else {
-      throw new BookNotFoundException({ bookId: id });
-    }
+  deleteBook(book: Book) {
+    this.books = this.books.filter((book) => book.id !== book.id);
+    return book;
   }
 
   updateBook(id: string, updateBookDto: UpdateBookDto) {
@@ -71,6 +58,5 @@ export class BooksStorageService {
       }
       return book;
     });
-    return this.getBook(id);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,55 +1,72 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
-import { StorageService } from '@app/storage';
 import { AuthorNotFoundException } from 'src/exceptions/author-not-found.exception';
 import { BookNotFoundException } from 'src/exceptions/book-not-found.exception';
+import { BooksStorageService } from '@app/books-storage';
+import { DuplicateRecordException } from 'src/exceptions/duplicate-record.exception';
 
 @Injectable()
 export class BooksService {
-  constructor(private readonly storageService: StorageService) {}
+  constructor(private readonly bookStorageService: BooksStorageService) {}
 
   create(createBookDto: CreateBookDto) {
-    return this.storageService.createBook(createBookDto);
+    // Check if book exists
+    const duplicate = this.bookStorageService.getBookByName(createBookDto.name);
+    if (duplicate) {
+      throw new DuplicateRecordException();
+    }
+    return this.bookStorageService.createBook(createBookDto);
   }
 
   findAll() {
-    return this.storageService.getBooks();
+    return this.bookStorageService.getBooks();
   }
 
   findOne(id: string) {
-    return this.storageService.getBook(id);
+    const book = this.bookStorageService.getBook(id);
+    if (!book) {
+      throw new BookNotFoundException({ bookId: id });
+    }
   }
 
   update(id: string, updateBookDto: UpdateBookDto) {
-    return this.storageService.updateBook(id, updateBookDto);
+    const toBeUpdated = this.bookStorageService.getBook(id);
+    if (!toBeUpdated) {
+      throw new BookNotFoundException({ bookId: id });
+    }
+    return this.bookStorageService.updateBook(id, updateBookDto);
   }
 
   remove(id: string) {
-    return this.storageService.deleteBook(id);
+    const toBeRemoved = this.bookStorageService.getBook(id);
+    if (!toBeRemoved) {
+      throw new BookNotFoundException({ bookId: id });
+    }
+    return this.bookStorageService.deleteBook(toBeRemoved);
   }
 
-  getAuthors(id: string) {
-    return this.storageService.getAuthorsFromBook(id);
-  }
-
-  linkAuthor(bookId: string, authorId: string) {
-    // Verify if the book exists
-    const book = this.storageService.getBook(bookId);
-    if (!book) {
+  //TODO: Implement with authorship service
+  /*
+    getAuthors(id: string) {
+      return this.bookStorageService.getAuthorsFromBook(id);
+    }
+  
+    linkAuthor(bookId: string, authorId: string) { // Verify if the book exists const book = this.bookStorageService.getBook(bookId); if (!book) {
       throw new BookNotFoundException({ bookId: bookId });
     }
-    const author = this.storageService.getAuthor(authorId);
-
+    const author = this.bookStorageService.getAuthor(authorId);
+  
     // Throw exception if author not found
-    if (!author) {
+    if(!author) {
       throw new AuthorNotFoundException({ authorId: authorId });
     }
-
-    return this.storageService.link(bookId, authorId);
-  }
-
+  
+      return this.bookStorageService.link(bookId, authorId);
+    }
+  
   removeLink(bookId: string, authorId: string) {
-    return this.storageService.unlink(bookId, authorId);
+    return this.bookStorageService.unlink(bookId, authorId);
   }
+  */
 }
